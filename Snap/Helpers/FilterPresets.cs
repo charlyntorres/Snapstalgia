bcc67cf8bb4a6c6e0dffb09c6f93e7127b53c546
charlyntorres <torrescharlynb@gmail.com>
2025-06-18 01:55:54 +0800

    edit photo v1

---
+++ Snap/Helpers/FilterPresets.cs
@@ -0,0 +1,21 @@
+﻿using SixLabors.ImageSharp.Processing;
+
+namespace Snap.Helpers
+{
+    public static class FilterPresets
+    {
+        public static IImageProcessingContext ApplyFilter(this IImageProcessingContext context, int? filterId)
+        {
+            if (!filterId.HasValue)          
+                return context;
+
+            return filterId.Value switch
+            {
+                1 => context.Grayscale(),
+                2 => context.Sepia(),
+                3 => context.Brightness(1.25f),
+                _ => context,
+            };
+        }
+    }
+}
---
+++ Snap/Helpers/StickerPresets.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Snap.Helpers
+{
+    public static class StickerPresets
+    {
+        private static readonly Dictionary<int, string> StickerMap = new()
+        {
+            { 1, "sparkle.png" },
+            { 2, "heart.png" },
+            { 3, "star.png" }            
+        };
+        
+        public static bool TryGetStickerPath(int? stickerId, out string relativePath)
+        {
+            relativePath = null;
+
+            if (!stickerId.HasValue || !StickerMap.TryGetValue(stickerId.Value, out var fileName))
+                return false;
+            
+            relativePath = Path.Combine("stickers", fileName);
+            return true;
+        }
+    }
+}
