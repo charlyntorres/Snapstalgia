98562720ce510981f8a2e38befabdde56c7a1bbd
angel-delossantos <angelmaydelossantos23@gmail.com>
2025-06-24 00:36:34 +0800

    Pushing about page

---
+++ Snap/wwwroot/js/faq.js
@@ -0,0 +1,27 @@
+﻿document.addEventListener("DOMContentLoaded", () => {
+    const faqItems = document.querySelectorAll(".faq-item");
+
+    faqItems.forEach(item => {
+        const question = item.querySelector(".faq-question");
+        const answer = item.querySelector(".faq-answer");
+        const toggle = item.querySelector(".faq-toggle");
+
+        question.addEventListener("click", () => {
+            const isOpen = item.classList.contains("open");
+
+            // Close all first
+            faqItems.forEach(i => {
+                i.classList.remove("open");
+                i.querySelector(".faq-toggle").textContent = "+";
+                const a = i.querySelector(".faq-answer");
+                a.style.maxHeight = "0";
+            });
+
+            if (!isOpen) {
+                item.classList.add("open");
+                toggle.textContent = "–";
+                answer.style.maxHeight = answer.scrollHeight + "px";
+            }
+        });
+    });
+});
